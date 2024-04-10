596741baae062717d4a31b54e6b11ecb564b5a8b
Johan Londoño <johan.londono@eholding.com.co>
2024-04-10 11:39:42 -0500

    test v2

--- app.js
+++ app.js
@@ -3,7 +3,33 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    const fs = require('fs').promises;
+    const find = req.query.find;
+    const procesoid = req.query.procesoid;
+
+    if (find === "getloghomologacion") {
+      const filePath = `jsons/loghomologacion_${procesoid}.json`;
+    
+      // Verifica la existencia del archivo
+      fs.access(filePath)
+        .then(() => fs.readFile(filePath, 'utf8'))
+        .then(data => {
+          const jsonData = JSON.parse(data);
+          jsonData.tipoDato = "Quemado";
+          res.json(jsonData);
+        })
+        .catch(err => {
+          console.error('Error al leer el archivo:', err);
+          if (err.code === 'ENOENT') {
+            res.status(404).send('Archivo no encontrado');
+          } else {
+            res.status(500).send('Error interno del servidor');
+          }
+        });
+    }
+    
+
+
 })
 
 app.listen(port, () => {
